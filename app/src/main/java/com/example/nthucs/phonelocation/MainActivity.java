6f76a2f1faf583ee16f84ab419bd6c497c86d566
Jason <j-lhy@hotmail.com>
2016-06-21 10:34:47 +0800

    add scedule manager, something has to be test on real device

--- app/src/main/java/com/example/nthucs/phonelocation/MainActivity.java
+++ app/src/main/java/com/example/nthucs/phonelocation/MainActivity.java
@@ -8,30 +8,36 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.location.Location;
 import android.location.LocationListener;
-import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.telephony.PhoneStateListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CONTACT = 1;
+    private ScheduleCallManager scheduleCallManager;
 
     @TargetApi(23)
-    public void requestPermissions(){
-        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CALL_PHONE}, 1000);
+    public void requestPermissions() {
+        // not backward compatible with API < 23
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CALL_PHONE}, 1000);
     }
 
-        @Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -50,21 +56,53 @@ public class MainActivity extends AppCompatActivity {
         final MainActivity theMainActivity = this;
         Button button = (Button) findViewById(R.id.button);
 
-        requestPermissions();
+        this.requestPermissions();
+
+        // listen to the phone state
+        MyPhoneStateListener mPhoneStateListener = new MyPhoneStateListener();
+        TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+        telephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        // my end call manager
+        ScheduleCallManager scheduleCallManager = new ScheduleCallManager();
+        if (!scheduleCallManager.initialize(telephonyManager, this)){
+            Toast.makeText(getApplicationContext(), "初始化EndCall Manager失敗", Toast.LENGTH_LONG).show();
+        }
+        this.scheduleCallManager = scheduleCallManager;
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LocationManager manager = (LocationManager) theMainActivity.getSystemService(Context.LOCATION_SERVICE);
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CONTACT);
+
+
+                // not working in my simulator comment out first
+                /* LocationManager manager = (LocationManager) theMainActivity.getSystemService(Context.LOCATION_SERVICE);
+              // List<String> providers =  manager.getAllProviders();
+
+
+                List<String> providers = manager.getProviders(true);
+                String bestProviders = null;
+                if (providers.contains(LocationManager.GPS_PROVIDER)){
+                    bestProviders = LocationManager.GPS_PROVIDER;
+                } else if (providers.contains(LocationManager.NETWORK_PROVIDER)){
+                    bestProviders = LocationManager.NETWORK_PROVIDER;
+                }
 
                 try {
-                    manager.requestSingleUpdate(LocationManager.GPS_PROVIDER, new MyLocationListener(), null);
+                    manager.requestSingleUpdate(bestProviders, new MyLocationListener(), null);
                 } catch (SecurityException ex) {
                     ex.printStackTrace();
-                }
+                } */
+
+
             }
         });
     }
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -107,8 +145,8 @@ public class MainActivity extends AppCompatActivity {
 
             Log.d("twlatitude", twlatitude + "");
             Log.d("twlongitude", twlongitude + "");
-            int [][] rect = Encode.encode(twlatitude, twlongitude);
-            for (int i = 0; i < rect.length; i++){
+            int[][] rect = Encode.encode(twlatitude, twlongitude);
+            for (int i = 0; i < rect.length; i++) {
                 for (int j = 0; j < rect[0].length; j++) {
                     System.out.println(rect[i][j] + " ");
                 }
@@ -119,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
             // makePhoneCall();
         }
 
-        public void pickContact(){
+        public void pickContact() {
             Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
             MainActivity.this.startActivityForResult(intent, REQUEST_CONTACT);
         }
@@ -144,42 +182,83 @@ public class MainActivity extends AppCompatActivity {
     public void onActivityResult(int reqCode, int resultCode, Intent data) {
         super.onActivityResult(reqCode, resultCode, data);
 
-        Log.d("activie result: ", resultCode + "");
-        if (resultCode == Activity.RESULT_OK){
-            switch (reqCode) {
-                case (REQUEST_CONTACT):
-                    if (resultCode == Activity.RESULT_OK) {
-                        Uri contactData = data.getData();
 
-                        Cursor c = getContentResolver().query(contactData, null, null, null, null);
-                        // Log.d("The column index for phone number", c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER) + "");
-                        // Log.d("all column name", c.getColumnNames().toString());
-                        Log.d("All column name: ", "Column name is listed in follwing");
-                        String [] columnNames = c.getColumnNames();
-                        for (int i = 0; i < c.getColumnCount(); i ++){
-                            Log.d("Column " + i, columnNames[i]);
-                        }
+        if (resultCode == Activity.RESULT_OK) {
+            Uri contactData = data.getData();
+            Cursor c = getContentResolver().query(contactData, null, null, null, null);
 
-                        if (c.moveToFirst()) {
-                            String name = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                            String number = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                            Log.d("contact naem: ", name + "");
-                            Log.d("contact number: ", number + "");
-                        }
-                        c.close();
-                    }
-                    break;
+            // the table returned from the built in contact list do not contain the number
+            // you have to retreive the phone number table using another uri...
+            if (c.moveToFirst()) {
+                String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                String contactName = c.getString(c.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                int hasPhoneNumber = c.getInt(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+
+                if (hasPhoneNumber == 1) {
+                    String[] selection = new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER};
+                    String filter = ContactsContract.CommonDataKinds.Phone.CONTACT_ID + "=" + contactId;
+                    Cursor phoneCursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, filter, null, null);
+                    phoneCursor.moveToNext();
+                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText("正在聯絡 " + contactName + "...");
+                    scheduleCallManager.setPhoneNumber(phoneNumber);
+                    scheduleCallManager.makeCall();
+                } else {
+                    Toast.makeText(getApplicationContext(), "請點選有至少一位電話號碼的聯絡人", Toast.LENGTH_LONG).show();
+                }
             }
+            c.close();
+        } else {
+            Toast.makeText(getApplicationContext(), "請點選聯絡人", Toast.LENGTH_LONG).show();
         }
     }
 
-    public void makePhoneCall(){
-        Intent intent = new Intent(Intent.ACTION_CALL);
-        intent.setData(Uri.parse("tel:09999"));
-        try {
-            startActivity(intent);
-        } catch (SecurityException ex){
-            ex.printStackTrace();
+    /**
+     *
+     */
+    public class MyPhoneStateListener extends PhoneStateListener {
+        private final int SIZE = 8;
+        private int [][] encoded;
+        private int i = 0;
+
+
+        public void setEncoded(int [][] encoded){
+            if (encoded.length != SIZE && encoded[0].length != SIZE){
+                throw new IllegalArgumentException("Encoded should be " + SIZE + " * " + SIZE);
+            }
+            this.encoded = encoded;
+            i = 0;
+        }
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            switch (state) {
+                case TelephonyManager.CALL_STATE_RINGING:
+                    Log.d("phone listener", "CALL_STATE_RINGIN");
+                    break;
+
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    Log.d("phone listener", "CALL_STATE_OFFHOOK");
+                    int content = encoded[i / SIZE][i % SIZE];
+                    if (content == 0) {
+                        scheduleCallManager.endCall(1000);
+                    } else {
+                        scheduleCallManager.endCall(10000);
+                    }
+                    break;
+
+                case TelephonyManager.CALL_STATE_IDLE:
+                    Log.d("phone listener", "CALL_STATE_IDLE");
+                    if (i < (SIZE * SIZE)){
+                        scheduleCallManager.makeCall(2000);
+                    }
+                    // successful
+                    i++;
+            }
+
+            super.onCallStateChanged(state, incomingNumber);
         }
     }
 
---
+++ app/src/main/java/com/example/nthucs/phonelocation/ScheduleCallManager.java
@@ -0,0 +1,104 @@
+package com.example.nthucs.phonelocation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.telephony.TelephonyManager;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Schedule outgoing and incoming call. It is assumed that the activity already have the permission
+ *
+ * Created by Jason on 6/21/16.
+ */
+public class ScheduleCallManager {
+    private Object endCallManager;
+    private Method endCallMethod;
+    private String phoneNumber;
+    private Activity activity;
+
+    /**
+     * Get the underlying endCallManager by Java reflection because it is an internal class inside telephonyManager....
+     *
+     * @return True if success, False if failed
+     */
+    public boolean initialize(TelephonyManager telephonyManager, Activity context) {
+        try {
+            Method getTelephonyManager = telephonyManager.getClass().getDeclaredMethod("getITelephony");
+            getTelephonyManager.setAccessible(true);
+            Object endCallManager = getTelephonyManager.invoke(telephonyManager);
+            Method endCallMethod = endCallManager.getClass().getDeclaredMethod("endCall");
+
+            this.endCallManager = endCallManager;
+            this.endCallMethod = endCallMethod;
+            this.activity = context;
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException ex ){
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * End the phone call
+     * @return True on Success, False on Failed
+     */
+    public boolean endCall(){
+        try {
+            endCallMethod.invoke(endCallManager);
+        } catch (InvocationTargetException | IllegalAccessException ex ){
+            ex.printStackTrace();
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * End call after delay second
+     *
+     * @param delay Second to be delayed
+     * @return
+     */
+    public void endCall(long delay){
+        Timer timer = new Timer(true);
+        timer.schedule(new EndCallTask(), delay);
+    }
+
+    class EndCallTask extends TimerTask {
+        public void run() {
+            ScheduleCallManager.this.endCall();
+        }
+    }
+
+    public void makeCall(){
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        try {
+            activity.startActivity(intent);
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void makeCall(long delay){
+        Timer timer = new Timer(true);
+        timer.schedule(new MakeCallTask(), delay);
+    }
+
+    class MakeCallTask extends TimerTask {
+        public void run() {
+            ScheduleCallManager.this.makeCall();
+        }
+    }
+
+}
